39d461eaf22b3ab63768695932cc3338e74279d7
Gonzalo Urroz <gonzalo.urroz@cursor.cl>
2018-12-27 23:16:56 +1100

    Changes configuration to prod server

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   baseURL: 'https://www.misionmujer.org/api/v1',
-  awsRegion: 'us-east-1',
-  awsBucketName: 'us-east-1',
+  awsRegion: 'us-west-1',
+  awsBucketName: 'misionmujer',
   awsIdentityPoolId: 'us-east-1:b4356ee4-f7df-4d6b-b484-51413cfb26de',
   optimizationUrl:  'https://us-central1-misionmujer-webadmin.cloudfunctions.net/image-optimizer'
 };
