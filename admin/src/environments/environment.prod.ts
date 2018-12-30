f74710c3234f519639294b7132471a0811c807d6
Gonzalo Urroz <gonzalo.urroz@cursor.cl>
2018-12-30 18:24:28 +1100

    Changes to s3 credentials

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   baseURL: 'https://www.misionmujer.org/api/v1',
-  awsRegion: 'us-west-1',
-  awsBucketName: 'misionmujer',
+  awsRegion: 'us-east-1',
+  awsBucketName: 'misionmujers3',
   awsIdentityPoolId: 'us-east-1:b4356ee4-f7df-4d6b-b484-51413cfb26de',
   optimizationUrl:  'https://us-central1-misionmujer-webadmin.cloudfunctions.net/image-optimizer'
 };
